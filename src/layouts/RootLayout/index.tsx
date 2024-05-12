e38d3fac4843563bd234227f02cf714af264a5dc
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2024-05-12 22:59:31 +0900

    add: scroll progress bar

--- src/layouts/RootLayout/index.tsx
+++ src/layouts/RootLayout/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect } from "react"
+import React, { useState, useEffect, useRef, ReactNode } from "react"
 import { ThemeProvider } from "./ThemeProvider"
 import useScheme from "src/hooks/useScheme"
 import Header from "./Header"
@@ -6,38 +6,38 @@ import styled from "@emotion/styled"
 import Scripts from "src/layouts/RootLayout/Scripts"
 import useGtagEffect from "./useGtagEffect"
 import Prism from "prismjs/prism"
-import 'prismjs/components/prism-markup-templating.js'
-import 'prismjs/components/prism-markup.js'
-import 'prismjs/components/prism-bash.js'
-import 'prismjs/components/prism-c.js'
-import 'prismjs/components/prism-cpp.js'
-import 'prismjs/components/prism-csharp.js'
-import 'prismjs/components/prism-docker.js'
-import 'prismjs/components/prism-java.js'
-import 'prismjs/components/prism-js-templates.js'
-import 'prismjs/components/prism-coffeescript.js'
-import 'prismjs/components/prism-diff.js'
-import 'prismjs/components/prism-git.js'
-import 'prismjs/components/prism-go.js'
-import 'prismjs/components/prism-kotlin.js'
-import 'prismjs/components/prism-graphql.js'
-import 'prismjs/components/prism-handlebars.js'
-import 'prismjs/components/prism-less.js'
-import 'prismjs/components/prism-makefile.js'
-import 'prismjs/components/prism-markdown.js'
-import 'prismjs/components/prism-objectivec.js'
-import 'prismjs/components/prism-ocaml.js'
-import 'prismjs/components/prism-python.js'
-import 'prismjs/components/prism-reason.js'
-import 'prismjs/components/prism-rust.js'
-import 'prismjs/components/prism-sass.js'
-import 'prismjs/components/prism-scss.js'
-import 'prismjs/components/prism-solidity.js'
-import 'prismjs/components/prism-sql.js'
-import 'prismjs/components/prism-stylus.js'
-import 'prismjs/components/prism-swift.js'
-import 'prismjs/components/prism-wasm.js'
-import 'prismjs/components/prism-yaml.js'
+import "prismjs/components/prism-markup-templating.js"
+import "prismjs/components/prism-markup.js"
+import "prismjs/components/prism-bash.js"
+import "prismjs/components/prism-c.js"
+import "prismjs/components/prism-cpp.js"
+import "prismjs/components/prism-csharp.js"
+import "prismjs/components/prism-docker.js"
+import "prismjs/components/prism-java.js"
+import "prismjs/components/prism-js-templates.js"
+import "prismjs/components/prism-coffeescript.js"
+import "prismjs/components/prism-diff.js"
+import "prismjs/components/prism-git.js"
+import "prismjs/components/prism-go.js"
+import "prismjs/components/prism-kotlin.js"
+import "prismjs/components/prism-graphql.js"
+import "prismjs/components/prism-handlebars.js"
+import "prismjs/components/prism-less.js"
+import "prismjs/components/prism-makefile.js"
+import "prismjs/components/prism-markdown.js"
+import "prismjs/components/prism-objectivec.js"
+import "prismjs/components/prism-ocaml.js"
+import "prismjs/components/prism-python.js"
+import "prismjs/components/prism-reason.js"
+import "prismjs/components/prism-rust.js"
+import "prismjs/components/prism-sass.js"
+import "prismjs/components/prism-scss.js"
+import "prismjs/components/prism-solidity.js"
+import "prismjs/components/prism-sql.js"
+import "prismjs/components/prism-stylus.js"
+import "prismjs/components/prism-swift.js"
+import "prismjs/components/prism-wasm.js"
+import "prismjs/components/prism-yaml.js"
 import "prismjs/components/prism-go.js"
 
 type Props = {
@@ -45,19 +45,38 @@ type Props = {
 }
 
 const RootLayout = ({ children }: Props) => {
+  const currentElementRef = useRef<HTMLDivElement | null>(null)
   const [scheme] = useScheme()
+  const [scrollPercentage, setScrollPercentage] = useState(0)
+
   useGtagEffect()
+
   useEffect(() => {
-    Prism.highlightAll();
-  }, []);
+    Prism.highlightAll()
+  }, [])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const windowHeight = window.innerHeight
+      const documentHeight = document.documentElement.scrollHeight
+      const scrollY = window.scrollY
+
+      const scrollPercent = (scrollY / (documentHeight - windowHeight)) * 100
+
+      setScrollPercentage(scrollPercent)
+    }
+
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
 
   return (
     <ThemeProvider scheme={scheme}>
       <Scripts />
       {/* // TODO: replace react query */}
       {/* {metaConfig.type !== "Paper" && <Header />} */}
-      <Header fullWidth={false} />
-      <StyledMain>{children}</StyledMain>
+      <Header fullWidth={false} readingProgress={scrollPercentage} />
+      <StyledMain ref={currentElementRef}>{children}</StyledMain>
     </ThemeProvider>
   )
 }
